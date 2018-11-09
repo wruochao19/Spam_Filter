922597a7d549769c1be627e6b9ea45fc69e142b5
wruochao19 <43077086+wruochao19@users.noreply.github.com>
2018-11-09 16:40:38 -0500

    the initail project

---
+++ Naive Bayes/__init__.py
@@ -0,0 +1,8 @@
+#! user/bin/pycharmPoject
+
+''' Spam Filter using Bayes '''
+
+__author__  = 'ruochao weng', 'yang li',''
+__email__   = 'wruochao19@outlook.com'
+__license__ = 'MIT License'
+__version__ = '0.1'
