8f52654ee63139d644b2ad5faec342e80961fcd3
FatNine <mdai1@stevens.edu>
2018-11-30 19:21:32 -0500

    error print
    print error email content

--- Naive Bayes/test.py
+++ Naive Bayes/test.py
@@ -19,20 +19,28 @@ def classfiyResult():
     mailNum = 1000
     true_mailType = []
     mailContent =[]
+    samp_list =[]
     for i in range(mailNum):
         sample = int(random.uniform(0,len(textWords)))
+        samp_list.append(sample)
         true_mailType.append(trainCategory[sample])
         mailContent.append(textWords[sample])
         del trainCategory[sample]
         del textWords[sample]
     vocableList, pWordsOfSpam, pWordsOfHam, pSpam = naiveBayes.getTrainInfo()
     errorCount = 0.0
+    err_list =[]
     for i in range(mailNum):
         mailType = naiveBayes.classify(vocableList,pWordsOfSpam, pWordsOfHam, pSpam, mailContent[i])
         print('forecast', mailType, 'true', true_mailType[i])
         if mailType != true_mailType[i]:
             errorCount += 1
+            err_list.append(i)
+    for i in err_list:
+        print('error email '+ str(i)+":")
+        print(textWords[samp_list[i]])
     print('errorNum', errorCount, 'errorRate', errorCount/mailNum)
+
 
 
 classfiyResult()
